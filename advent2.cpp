f22b9409c6437413ca3597d3222585f763df2c76
questionc <questionc@users.noreply.github.com>
2018-03-12 14:51:46 -0400

    Day2 Star2

--- advent2.cpp
+++ advent2.cpp
@@ -1,9 +1,28 @@
 // For each row in input2.txt, find the difference between the max and min value
 // Return the sum of the max and min value for each line
 
+// For the second star, the checksum is the division of the only 2 numbers that evenly divide in each line
+
 #include <fstream>
 #include <iostream>
 #include <sstream>
+#include <vector>
+
+// See if N divides any number in the vector or vice versa and return the division.
+// Return 0 if there is no division
+int divides_check(int N, std::vector<int> & V) {
+    std::vector<int>::const_iterator ci;
+    for (ci = V.begin(); ci != V.end(); ++ci) {
+        if (N % *ci == 0) {
+            return N / *ci;
+        }
+        else if (*ci % N == 0) {
+            return *ci / N;
+        }
+    }
+
+    return 0;
+}
 
 int main(void) {
     std::ifstream input("input2.txt");
@@ -13,20 +32,22 @@ int main(void) {
     while(std::getline(input, line)) {
         std::stringstream stream(line);
 
-        int max, min;
-        stream >> max;
-        min = max;
+        std::vector<int> values;
 
         int curr_num;
         while (stream >> curr_num) {
-            if (max < curr_num) {
-                max = curr_num;
+            int N = divides_check(curr_num, values);
+            if (N == 0) {
+                values.push_back(curr_num);
             }
-            else if (min > curr_num) {
-                min = curr_num;
+            else { // Success!
+                checksum_total += N;
+                break;
             }
         }
-        checksum_total += max - min;
+        if (!stream) {
+            std::cerr << "We got to the end of stream without dividing!\n";
+        }
     }
 
     std::cout << checksum_total;
