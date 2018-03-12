97bbd22a0471973d7fab5e37742c51e6e8fc6820
questionc <questionc@users.noreply.github.com>
2018-03-12 18:39:59 -0400

    Day4 Star2

--- advent4.cpp
+++ advent4.cpp
@@ -3,6 +3,7 @@
 // Find the number of lines in input4.txt with no duplicate words
 
 #include <unordered_set>
+#include <algorithm>
 
 int main (void) {
     std::ifstream input("input4.txt");
@@ -18,6 +19,10 @@ int main (void) {
 
         bool valid_pass = true;
         while (stream >> curr_word) {
+            // If we sort the word before inserting it catches anagrams
+            std::sort(curr_word.begin(), curr_word.end());
+
+
             DPRINT(curr_word);
             if (seen_words.find(curr_word) != seen_words.end()) {
                 valid_pass = false;
