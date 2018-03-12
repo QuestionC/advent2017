608507deb1a32ca3012bf6f2878b59819a41a4a4
questionc <questionc@users.noreply.github.com>
2018-03-12 14:34:56 -0400

    Solved Day 1 Star 2

--- advent1.cpp
+++ advent1.cpp
@@ -10,22 +10,15 @@ int main (void) {
     std::string str;
     std::getline(file, str);
 
-    char const * c_str = str.c_str();
-
     int repeats = 0;
-    for (char const * ch = c_str; *ch != '\0' && *ch != '\n'; ++ch) {
-        char const * next = ch + 1;
-        if (*next != '\0' && *next != '\n') {
-            if (*ch == *next) {
-                // std::cout << *ch << " match in position " << ch - c_str << '\n';
-                repeats += *ch - '0';
-            }
-        }
-        else { // end of string
-            if (*ch == c_str[0]) {
-                // std::cout << *ch << " match at end" << '\n';
-                repeats += *ch - '0';
-            }
+    int len = str.length();
+    int index = 0;
+
+    int i;
+    int compare;
+    for (i = 0, compare = len / 2; i < len; ++i, compare = (compare + 1) % len) {
+        if (str[i] == str[compare]) {
+            repeats += str[i] - '0';
         }
     }
 
